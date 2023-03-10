e6d8f5f821125655fb3f5927ed0cb4299fb11d82
Petrov Konstantin <multuminparvoo>
2023-03-11 01:59:51 +0300

    fix setup

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path, include
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
 from .views import (
     SignupApiView, GetTokenApiView, UsersViewSet, CategoryViewSet,
     GenreViewSet, TitleViewSet, ReviewViewSet, CommentViewSet,)
-
-from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('users', UsersViewSet, basename='users')
